961b5a0c8265396936f9762de13b6372728f25d7
larry <wang.zhen.hua.larry@gmail.com>
2017-07-22 18:25:37 +0800

    update two sum algorithm

--- [MOOC]Algorithms graphs data structures/WEEK4 Hashing/two_sum.py
+++ [MOOC]Algorithms graphs data structures/WEEK4 Hashing/two_sum.py
@@ -1,68 +1,63 @@
-
-def two_sum_count(numbers, low, high):
-	"""
-	Compute the number of target values t in the interval[low, high] (inclusive),
-	such that there are distinct numbers x, y that satisfy x+y = t
-	"""
-	counts = 0
-
-	# choose a number
-	for number in numbers.keys():
-
-		# confine another number
-		for another_number in range(-number+low, -number+high+1):
-			# check if it exists
-			if another_number in numbers:
-					counts += 1
-
-	return counts
-
-
 class HashTable(object):
 
 	def __init__(self):
 
-		self.size = 5E6
-		self.slots = [set() * self.size]
+		self.size = 5000000
+		self.slots = [set() for i in range(self.size)]
+		self.data = set()
 
 	def put(self, key, data):
 
 		hash_value = self.hash_function(key)
-		if self.slots[hash_value]:
-			self.slots[hash_value] = data
-		else:
-			self.slots[hash_value].add(data)
+		self.slots[hash_value].add(data)
+		self.data.add(data)
 
 	def hash_function(self, key):
 
-		return key//(self.size/2)
+		return key//(self.size//2)
 
 	def get(self, key):
 		"""
 		there key == data
 		"""
-		return key
+		slot = self.hash_function(key)
+		if key in self.slots[slot]:
+			return key
+		else:
+			return None
 
-	def __contains__(self, item):
+	def buildfromfile(self, textfile):
 
-		for keyset in self.slots:
-			for key in keyset:
-				if key == item:
-					return True
+		with open(text_file, 'r') as infile:
+			for line in infile.readlines():
+				self.put(int(line), int(line))
 
-		else False
+	def two_sum_count(self):
+		"""
+		Compute the number of target values t in the interval[low, high] (inclusive),
+		such that there are distinct numbers x, y that satisfy x+y = t
+		"""
+		sums = set()
+
+		for key1 in self.data:
+			left = self.hash_function(-key1-10000)
+			right = self.hash_function(-key1+10000)
+
+			for slot in self.slots[left:right+1]:
+
+				for key2 in slot:
+
+					if 10000 > key1 + key2 > -10000:
+						sums.add(key1+key2)
+
+		return len(sums)
+
 
 if __name__ == "__main__":
 
 	# target files for counting
 	text_file = 'two_sum.txt'
 
-	numbers = {}
-
-	with open(text_file, 'r') as infile:
-		for line in infile.readlines():
-			numbers[int(line)] = 1
-
-
-
-	print(two_sum_count(numbers, -10000, 10000))
+	hashnumber = HashTable()
+	hashnumber.buildfromfile(text_file)
+	print(hashnumber.two_sum_count())
