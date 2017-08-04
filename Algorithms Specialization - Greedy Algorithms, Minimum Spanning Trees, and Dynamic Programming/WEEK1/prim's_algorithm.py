60903816c0fe59f8e7eaaf2387232738ea399017
larry <wang.zhen.hua.larry@gmail.com>
2017-08-04 16:10:17 +0800

    update prim's algorithm: return the edges of minimum spanning tree, not the total cost of its edge weight

--- Algorithms Specialization - Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/WEEK1/prim's_algorithm.py
+++ Algorithms Specialization - Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/WEEK1/prim's_algorithm.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/users/larry/Algorithm')
 from priorityqueue import PriorityQueue
 from graph import Graph
 import unittest
@@ -9,26 +11,33 @@ def prim(agraph, start):
     Prim's algorithm for minimum spanning tree
     Using min-heap data structure
 
-    return the overall cost of a minimum spanning tre
+    return a minimum spanning tree
     """
+    # vertex of minimun spanning tree
+    mst_vertex = []
     pq = PriorityQueue()
-    total_cost = 0
     for v in agraph:
         v.setDistance(sys.maxsize)
         v.setPred(None)
     start.setDistance(0)
     pq.buildHeap([(v.getDistance(), v) for v in agraph])
     while not pq.isEmpty():
-        cost, u = pq.delMin()
-        total_cost += cost
+        u = pq.delMin()
+        mst_vertex.append(u)
         for adjacent in u.getConnections():
-          newcost = u.getWeight(adjacent)
-          if adjacent in pq and newcost < adjacent.getDistance():
-              adjacent.setPred(u)
-              adjacent.setDistance(newcost)
-              pq.decreaseKey(adjacent, newcost)
+            newcost = u.getWeight(adjacent)
+            if adjacent in pq and newcost < adjacent.getDistance():
+                adjacent.setPred(u)
+                adjacent.setDistance(newcost)
+                pq.decreaseKey(adjacent, newcost)
 
-    return total_cost
+    # edges of minimum spanning tree
+    mst = []
+    for i in range(1, len(mst_vertex)):
+        # u, v, cost
+        mst.append((mst_vertex[i-1], mst_vertex[i],  mst_vertex[i].getDistance()))
+
+    return mst
 
 
 class TestPrim(unittest.TestCase):
@@ -49,7 +58,11 @@ class TestPrim(unittest.TestCase):
                     agraph.addEdge(to_u, from_v, cost)
                 count += 1
 
-        self.assertEqual(prim(agraph, agraph.getVertex(1)), -3612829)
+        total_cost = 0
+        mst = prim(agraph, agraph.getVertex(1))
+        for u, v, cost in mst:
+            total_cost += cost
+        self.assertEqual(total_cost, -3612829)
 
     def test_small(self):
         agraph = Graph()
@@ -64,7 +77,11 @@ class TestPrim(unittest.TestCase):
                     agraph.addEdge(to_u, from_v, cost)
                 count += 1
 
-        self.assertEqual(prim(agraph, agraph.getVertex(1)), 14)
+        total_cost = 0
+        mst = prim(agraph, agraph.getVertex(1))
+        for u, v, cost in mst:
+            total_cost += cost
+        self.assertEqual(total_cost, 14)
 
 
 if __name__ == "__main__":
