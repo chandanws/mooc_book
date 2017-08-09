b1b11554ed73852bd9ca906053679140cca28392
larry <wang.zhen.hua.larry@gmail.com>
2017-08-10 02:38:21 +0800

    put recursive function inside

--- Algorithms Specialization - Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/WEEK4/knapsack_recursive.py
+++ Algorithms Specialization - Greedy Algorithms, Minimum Spanning Trees, and Dynamic Programming/WEEK4/knapsack_recursive.py
@@ -4,36 +4,46 @@ import sys
 sys.setrecursionlimit(3000)
 
 
-def knapsack01_recursive(hash_table, value, weight, i, j):
-	# base case: when 1 items here
-	if i == 1:
-		if weight[i - 1] < j:
-			hash_table[(i, j)] = value[i - 1]
-			return value[i - 1]
-		else:
-			hash_table[(i, j)] = 0
-		return 0
+def knapSack(val, wt, capacity):
+	"""
+	Recursive implementation of 0-1 Knapsack Problem
 
-	# sub-problem computed
-	if (i, j) in hash_table:
-		return hash_table[(i, j)]
-	else:
-		if weight[i - 1] > j:
-			# only case 1
-			hash_table[(i, j)] = knapsack01_recursive(hash_table, value, weight, i - 1, j)
-		else:
-			#  case 1, item i-1 excluded
-			case1 = knapsack01_recursive(hash_table, value, weight, i - 1, j)
-			# case 2, item i-1 included
-			case2 = knapsack01_recursive(hash_table, value, weight, i - 1, j - weight[i - 1]) + value[i - 1]
-			hash_table[(i, j)] = max(case1, case2)
+	val: value
+	wt: weight
+	capacity: capacity
+	"""
 
-	return hash_table[(i, j)]
+	def max_value(i, j):
+		# base case
+		if i == 0:
+			return 0
+
+		# sub-problem computed
+		if (i, j) in hash_table:
+			return hash_table[(i, j)]
+		else:
+			if wt[i - 1] > j:
+				# only case 1
+				max_val = max_value(i - 1, j)
+				hash_table[(i, j)] = max_val
+			else:
+				#  case 1, item i-1 excluded
+				case1 = max_value(i - 1, j)
+				# case 2, item i-1 included
+				case2 = max_value(i - 1, j - wt[i - 1]) + val[i - 1]
+				max_val = max(case1, case2)
+				hash_table[(i, j)] = max_val
+
+			return max_val
+
+	hash_table = {}
+	return max_value(len(val), capacity)
+
 
 
 class TestKnapsack(unittest.TestCase):
 
-	def tests_final(self):
+	def notests_final(self):
 		values = []
 		weights = []
 		with open('input_random_44_2000000_2000.txt') as infile:
@@ -48,10 +58,10 @@ class TestKnapsack(unittest.TestCase):
 		if n != len(values):
 			raise ValueError
 
-		max_val = knapsack01_recursive({}, values, weights, n, capacity)
+		max_val = knapSack({}, values, weights, n, capacity)
 		self.assertEqual(max_val, 49957183)
 
-	def notests_large(self):
+	def test_large(self):
 		values = []
 		weights = []
 		with open('input_random_29_10000_1000.txt') as infile:
@@ -66,7 +76,7 @@ class TestKnapsack(unittest.TestCase):
 		if n != len(values):
 			raise ValueError
 
-		max_val = knapsack01_recursive({}, values, weights, n, capacity)
+		max_val = knapSack(values, weights, capacity)
 		self.assertEqual(max_val, 167375)
 
 	def notests_big(self):
@@ -84,7 +94,7 @@ class TestKnapsack(unittest.TestCase):
 		if n != len(values):
 			raise ValueError
 
-		max_val = knapsack01_recursive({}, values, weights, n, capacity)
+		max_val = knapSack(values, weights, capacity)
 		self.assertEqual(max_val, 1580)
 
 	def notests_small(self):
@@ -102,7 +112,7 @@ class TestKnapsack(unittest.TestCase):
 		if n != len(values):
 			raise ValueError
 
-		max_val = knapsack01_recursive({}, values, weights, n, capacity)
+		max_val = knapSack(values, weights, capacity)
 		self.assertEqual(max_val, 8)
 
 
