a720df87fd41581b2ef73bbbc886bd9f3a21f8e7
larry <wang.zhen.hua.larry@gmail.com>
2017-11-11 20:15:31 +0800

    add Openjudge

---
+++ C++程序设计/封闭类例子程序.cpp
@@ -0,0 +1,8 @@
+//
+//  封闭类例子程序.cpp
+//  
+//
+//  Created by larry on 11/11/2017.
+//
+
+#include <stdio.h>
---
+++ OpenJudge/5:整型数据类型存储空间大小.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	short int b;
+	cout << sizeof(a) << " "  << sizeof(b) << endl;
+}
+
